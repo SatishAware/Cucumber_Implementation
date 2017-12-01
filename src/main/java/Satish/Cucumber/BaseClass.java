b18931d338ab80c00c9f0e005d7833b276c8c3f0
SATISH <SATISH@DESKTOP-1RJN9BM>
2017-12-02 03:03:29 +0530

    Initial Code

---
+++ src/main/java/Satish/Cucumber/BaseClass.java
@@ -0,0 +1,14 @@
+package Satish.Cucumber;
+
+import org.openqa.selenium.WebDriver;
+
+public abstract class BaseClass {
+	public static WebDriver driver;
+	public static boolean bResult;
+
+	public  BaseClass(WebDriver driver){
+		BaseClass.driver = driver;
+		BaseClass.bResult = true;
+	}
+
+}
